aba092c3504a5c9dd23cadb234011a7829444cdf
LeeDMR <dimlee.18@gmail.com>
2024-02-14 20:07:39 +0600

    1

---
+++ lab3/task1/objects.js
@@ -0,0 +1,76 @@
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
+
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  
+  alert(sum);
+
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  multiplyNumeric(menu);
+  
+  menu = {
+    width: 400,
+    height: 600,
+    title: "My menu"
+  };
+
+  let calculator = {
+    sum() {
+      return this.a + this.b;
+    },
+  
+    mul() {
+      return this.a * this.b;
+    },
+  
+    read() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
+
+  let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      alert( this.step );
+      return this;
+    }
+  };
+  
+  ladder.up().up().down().showStep().down().showStep();
